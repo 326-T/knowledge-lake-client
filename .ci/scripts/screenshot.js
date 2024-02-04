0ce25eac5c072347f04e2f8442328d6cb5cbc6dc
Mitsuru Takeda <m.t.hagaren1@gmail.com>
2024-02-04 22:20:45 +0900

    [update]画面のロードを待つように設定

--- .ci/scripts/screenshot.js
+++ .ci/scripts/screenshot.js
@@ -4,18 +4,22 @@ const puppeteer = require("puppeteer");
   // ブラウザインスタンスを起動
   const browser = await puppeteer.launch({
     args: ["--no-sandbox", "--disable-setuid-sandbox"],
-    headless: true,
+    headless: "new",
   });
   const page = await browser.newPage();
 
   await page.setViewport({ width: 1920, height: 1080 });
 
   // 対象のURLにアクセス
-  await page.goto(`http://${process.env.HOSTNAME}:3000/library`);
+  await page.goto(`http://host.docker.internal:3001/library`, {
+    waitUntil: "networkidle0",
+  });
   await page.screenshot({ path: "library_HD.png" });
 
   // 対象のURLにアクセス
-  await page.goto(`http://${process.env.HOSTNAME}:3000/talk-with-ai`);
+  await page.goto(`http://host.docker.internal:3001/talk-with-ai`, {
+    waitUntil: "networkidle0",
+  });
   await page.screenshot({ path: "talk-with-ai_HD.png" });
 
   // スマホサイズに変更
