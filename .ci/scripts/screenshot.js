298dbd1560ff358f01c8a641767ffb19c19e9929
Mitsuru Takeda <m.t.hagaren1@gmail.com>
2024-02-05 00:41:26 +0900

    [add]agentの設定を追加してみた

--- .ci/scripts/screenshot.js
+++ .ci/scripts/screenshot.js
@@ -11,7 +11,9 @@ import puppeteer from "https://deno.land/x/puppeteer@16.2.0/mod.ts";
     headless: "new",
   });
   const page = await browser.newPage();
-
+  page.setUserAgent(
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36"
+  );
   await page.setViewport({ width: 1920, height: 1080 });
 
   // 対象のURLにアクセス
