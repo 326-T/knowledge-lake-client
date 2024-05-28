1914247eaf9e5ee0c326a1bbbd1de14e3a2c9958
Mitsuru Takeda <m.t.hagaren1@gmail.com>
2024-05-28 22:20:08 +0900

    [fix]jsファイルの修正

--- .ci/scripts/screenshot.js
+++ .ci/scripts/screenshot.js
@@ -1,17 +1,16 @@
 import puppeteer from "https://deno.land/x/puppeteer@16.2.0/mod.ts";
-import puppeteer from "puppeteer";
 
 (async () => {
   const args = process.argv.slice(2); // Get command-line arguments excluding "node" and "screenshot.js"
-  const prefix = args[0]; // Get the first argument as the path to save the screenshot
-
+  const pathToSave = args[0]; // Get the first argument as the path to save the screenshot
+  // ブラウザインスタンスを起動
   const browser = await puppeteer.launch({
     args: [
       "--no-sandbox",
       "--disable-setuid-sandbox",
       "--disable-dev-shm-usage",
     ],
-    headless: true,
+    headless: "new",
   });
   const page = await browser.newPage();
   page.setUserAgent(
@@ -20,10 +19,12 @@ import puppeteer from "puppeteer";
 
   await page.setViewport({ width: 1920, height: 1080 });
 
+  // 対象のURLにアクセス
   await page.goto("http://localhost:3000/library", {
     waitUntil: "networkidle0",
   });
-  await page.screenshot({ path: `${prefix}_library_HD.png` });
+  await page.screenshot({ path: `${pathToSave}_library_HD.png` });
 
+  // ブラウザを閉じる
   await browser.close();
 })();
